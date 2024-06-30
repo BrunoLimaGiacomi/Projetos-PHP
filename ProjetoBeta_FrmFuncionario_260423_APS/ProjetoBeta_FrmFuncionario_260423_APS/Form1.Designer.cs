c3fbc89a9567d8df84da4a64cdcf0e4cf648d970
Bruno Lima Giacomi <67843103+BrunoLimaGiacomi@users.noreply.github.com>
2024-06-30 17:04:52 -0300

    Add files via upload

---
+++ ProjetoBeta_FrmFuncionario_260423_APS/ProjetoBeta_FrmFuncionario_260423_APS/Form1.Designer.cs
@@ -0,0 +1,389 @@
+﻿namespace ProjetoBeta_FrmFuncionario_260423_APS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GrpFuncionario = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.BtnIncluir = new System.Windows.Forms.Button();
+            this.BtnAlterar = new System.Windows.Forms.Button();
+            this.BtnExcluir = new System.Windows.Forms.Button();
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.BtnLocalizar = new System.Windows.Forms.Button();
+            this.txtIdFunc = new System.Windows.Forms.TextBox();
+            this.txtNomeFunc = new System.Windows.Forms.TextBox();
+            this.txtEndFunc = new System.Windows.Forms.TextBox();
+            this.txtCpfFunc = new System.Windows.Forms.MaskedTextBox();
+            this.txtDtaNascFunc = new System.Windows.Forms.MaskedTextBox();
+            this.txtDtaAdmFunc = new System.Windows.Forms.MaskedTextBox();
+            this.TxtFoneFunc = new System.Windows.Forms.MaskedTextBox();
+            this.txtRGFunc = new System.Windows.Forms.MaskedTextBox();
+            this.CbCargoFunc = new System.Windows.Forms.ComboBox();
+            this.CbStatusFunc = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtSenhaFunc = new System.Windows.Forms.TextBox();
+            this.GrpFuncionario.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GrpFuncionario
+            // 
+            this.GrpFuncionario.Controls.Add(this.txtSenhaFunc);
+            this.GrpFuncionario.Controls.Add(this.label11);
+            this.GrpFuncionario.Controls.Add(this.CbStatusFunc);
+            this.GrpFuncionario.Controls.Add(this.CbCargoFunc);
+            this.GrpFuncionario.Controls.Add(this.txtRGFunc);
+            this.GrpFuncionario.Controls.Add(this.TxtFoneFunc);
+            this.GrpFuncionario.Controls.Add(this.txtDtaAdmFunc);
+            this.GrpFuncionario.Controls.Add(this.txtDtaNascFunc);
+            this.GrpFuncionario.Controls.Add(this.txtCpfFunc);
+            this.GrpFuncionario.Controls.Add(this.txtEndFunc);
+            this.GrpFuncionario.Controls.Add(this.txtNomeFunc);
+            this.GrpFuncionario.Controls.Add(this.txtIdFunc);
+            this.GrpFuncionario.Controls.Add(this.BtnLocalizar);
+            this.GrpFuncionario.Controls.Add(this.label10);
+            this.GrpFuncionario.Controls.Add(this.label9);
+            this.GrpFuncionario.Controls.Add(this.label8);
+            this.GrpFuncionario.Controls.Add(this.label7);
+            this.GrpFuncionario.Controls.Add(this.label6);
+            this.GrpFuncionario.Controls.Add(this.label5);
+            this.GrpFuncionario.Controls.Add(this.label4);
+            this.GrpFuncionario.Controls.Add(this.label3);
+            this.GrpFuncionario.Controls.Add(this.label2);
+            this.GrpFuncionario.Controls.Add(this.label1);
+            this.GrpFuncionario.Location = new System.Drawing.Point(9, 15);
+            this.GrpFuncionario.Name = "GrpFuncionario";
+            this.GrpFuncionario.Size = new System.Drawing.Size(369, 266);
+            this.GrpFuncionario.TabIndex = 0;
+            this.GrpFuncionario.TabStop = false;
+            this.GrpFuncionario.Text = "Dados do Funcionário";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fone:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(202, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Status:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Cargo: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(220, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Rg;";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 93);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Endereço:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(186, 127);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 15);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Dta.Adm.:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(213, 30);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(31, 15);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "CPF:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(0, 123);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(63, 15);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Dta. Nasc.:";
+            // 
+            // BtnIncluir
+            // 
+            this.BtnIncluir.Location = new System.Drawing.Point(33, 286);
+            this.BtnIncluir.Name = "BtnIncluir";
+            this.BtnIncluir.Size = new System.Drawing.Size(75, 23);
+            this.BtnIncluir.TabIndex = 1;
+            this.BtnIncluir.Text = "Incluir";
+            this.BtnIncluir.UseVisualStyleBackColor = true;
+            // 
+            // BtnAlterar
+            // 
+            this.BtnAlterar.Location = new System.Drawing.Point(119, 287);
+            this.BtnAlterar.Name = "BtnAlterar";
+            this.BtnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.BtnAlterar.TabIndex = 2;
+            this.BtnAlterar.Text = "Alterar";
+            this.BtnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // BtnExcluir
+            // 
+            this.BtnExcluir.Location = new System.Drawing.Point(204, 288);
+            this.BtnExcluir.Name = "BtnExcluir";
+            this.BtnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.BtnExcluir.TabIndex = 3;
+            this.BtnExcluir.Text = "Excluir";
+            this.BtnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.Location = new System.Drawing.Point(294, 289);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(75, 23);
+            this.BtnSair.TabIndex = 4;
+            this.BtnSair.Text = "Sair";
+            this.BtnSair.UseVisualStyleBackColor = true;
+            // 
+            // BtnLocalizar
+            // 
+            this.BtnLocalizar.Location = new System.Drawing.Point(127, 26);
+            this.BtnLocalizar.Name = "BtnLocalizar";
+            this.BtnLocalizar.Size = new System.Drawing.Size(75, 23);
+            this.BtnLocalizar.TabIndex = 10;
+            this.BtnLocalizar.Text = "Localizar";
+            this.BtnLocalizar.UseVisualStyleBackColor = true;
+            // 
+            // txtIdFunc
+            // 
+            this.txtIdFunc.Location = new System.Drawing.Point(69, 25);
+            this.txtIdFunc.Name = "txtIdFunc";
+            this.txtIdFunc.Size = new System.Drawing.Size(52, 23);
+            this.txtIdFunc.TabIndex = 11;
+            // 
+            // txtNomeFunc
+            // 
+            this.txtNomeFunc.Location = new System.Drawing.Point(69, 55);
+            this.txtNomeFunc.Name = "txtNomeFunc";
+            this.txtNomeFunc.Size = new System.Drawing.Size(272, 23);
+            this.txtNomeFunc.TabIndex = 12;
+            // 
+            // txtEndFunc
+            // 
+            this.txtEndFunc.Location = new System.Drawing.Point(69, 90);
+            this.txtEndFunc.Name = "txtEndFunc";
+            this.txtEndFunc.Size = new System.Drawing.Size(272, 23);
+            this.txtEndFunc.TabIndex = 13;
+            // 
+            // txtCpfFunc
+            // 
+            this.txtCpfFunc.Location = new System.Drawing.Point(250, 25);
+            this.txtCpfFunc.Mask = "000,000,000-00";
+            this.txtCpfFunc.Name = "txtCpfFunc";
+            this.txtCpfFunc.Size = new System.Drawing.Size(91, 23);
+            this.txtCpfFunc.TabIndex = 14;
+            // 
+            // txtDtaNascFunc
+            // 
+            this.txtDtaNascFunc.Location = new System.Drawing.Point(68, 124);
+            this.txtDtaNascFunc.Mask = "00/00/0000";
+            this.txtDtaNascFunc.Name = "txtDtaNascFunc";
+            this.txtDtaNascFunc.Size = new System.Drawing.Size(100, 23);
+            this.txtDtaNascFunc.TabIndex = 15;
+            this.txtDtaNascFunc.ValidatingType = typeof(System.DateTime);
+            // 
+            // txtDtaAdmFunc
+            // 
+            this.txtDtaAdmFunc.Location = new System.Drawing.Point(250, 123);
+            this.txtDtaAdmFunc.Mask = "00/00/0000";
+            this.txtDtaAdmFunc.Name = "txtDtaAdmFunc";
+            this.txtDtaAdmFunc.Size = new System.Drawing.Size(91, 23);
+            this.txtDtaAdmFunc.TabIndex = 16;
+            this.txtDtaAdmFunc.ValidatingType = typeof(System.DateTime);
+            // 
+            // TxtFoneFunc
+            // 
+            this.TxtFoneFunc.Location = new System.Drawing.Point(69, 154);
+            this.TxtFoneFunc.Mask = "(00) 0-0000-0000";
+            this.TxtFoneFunc.Name = "TxtFoneFunc";
+            this.TxtFoneFunc.Size = new System.Drawing.Size(100, 23);
+            this.TxtFoneFunc.TabIndex = 17;
+            // 
+            // txtRGFunc
+            // 
+            this.txtRGFunc.Location = new System.Drawing.Point(250, 154);
+            this.txtRGFunc.Name = "txtRGFunc";
+            this.txtRGFunc.Size = new System.Drawing.Size(92, 23);
+            this.txtRGFunc.TabIndex = 18;
+            // 
+            // CbCargoFunc
+            // 
+            this.CbCargoFunc.FormattingEnabled = true;
+            this.CbCargoFunc.Items.AddRange(new object[] {
+            "Atendente",
+            "Caixa",
+            "Faxineiro",
+            "Gerente",
+            "Garçom",
+            "Motoboy",
+            "Pizzaiolo",
+            "Segurança"});
+            this.CbCargoFunc.Location = new System.Drawing.Point(69, 183);
+            this.CbCargoFunc.Name = "CbCargoFunc";
+            this.CbCargoFunc.Size = new System.Drawing.Size(100, 23);
+            this.CbCargoFunc.TabIndex = 19;
+            // 
+            // CbStatusFunc
+            // 
+            this.CbStatusFunc.FormattingEnabled = true;
+            this.CbStatusFunc.Items.AddRange(new object[] {
+            "Afastado",
+            "Aposentado",
+            "Desativado",
+            "Efetivo",
+            "Licenciado",
+            "Temporário"});
+            this.CbStatusFunc.Location = new System.Drawing.Point(250, 184);
+            this.CbStatusFunc.Name = "CbStatusFunc";
+            this.CbStatusFunc.Size = new System.Drawing.Size(92, 23);
+            this.CbStatusFunc.TabIndex = 20;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(11, 224);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(42, 15);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Senha:";
+            // 
+            // txtSenhaFunc
+            // 
+            this.txtSenhaFunc.Location = new System.Drawing.Point(68, 223);
+            this.txtSenhaFunc.Name = "txtSenhaFunc";
+            this.txtSenhaFunc.Size = new System.Drawing.Size(100, 23);
+            this.txtSenhaFunc.TabIndex = 22;
+            this.txtSenhaFunc.UseSystemPasswordChar = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 339);
+            this.Controls.Add(this.BtnSair);
+            this.Controls.Add(this.BtnExcluir);
+            this.Controls.Add(this.BtnAlterar);
+            this.Controls.Add(this.BtnIncluir);
+            this.Controls.Add(this.GrpFuncionario);
+            this.Name = "Form1";
+            this.Text = "Cadastro de Funcionário";
+            this.GrpFuncionario.ResumeLayout(false);
+            this.GrpFuncionario.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox GrpFuncionario;
+        private TextBox txtSenhaFunc;
+        private Label label11;
+        private ComboBox CbStatusFunc;
+        private ComboBox CbCargoFunc;
+        private MaskedTextBox txtRGFunc;
+        private MaskedTextBox TxtFoneFunc;
+        private MaskedTextBox txtDtaAdmFunc;
+        private MaskedTextBox txtDtaNascFunc;
+        private MaskedTextBox txtCpfFunc;
+        private TextBox txtEndFunc;
+        private TextBox txtNomeFunc;
+        private TextBox txtIdFunc;
+        private Button BtnLocalizar;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Button BtnIncluir;
+        private Button BtnAlterar;
+        private Button BtnExcluir;
+        private Button BtnSair;
+    }
+}
---
+++ ProjetoBeta_FrmFuncionario_260423_APS/ProjetoBeta_FrmFuncionario_260423_APS/Form1.cs
@@ -0,0 +1,10 @@
+namespace ProjetoBeta_FrmFuncionario_260423_APS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
